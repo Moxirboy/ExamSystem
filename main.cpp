ef94739cf16898332bfa5946b9867a8b507f836f
nuriddinkamoldinov <n.kamoldinov@student.inha.uz>
2023-04-19 11:59:44 +0500

    Removed .vs, refactor

--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
-#include<iostream>
-#include<string>
-#include<fstream>
+#include <iostream>
+#include <string>
+#include <fstream>
 using namespace std;
-class person{
-private:
-    string ID;
-    string password;
-public:
-person(string id,string password){
-    ID=id;
-    this->password=password;
-    
-}
-};
-int main(){
-    cout<<"boshlimizmi???????????";
+
+int main()
+{
+    cout << "Everything is READY guys !" << endl;
     system("pause");
     return 0;
 }
