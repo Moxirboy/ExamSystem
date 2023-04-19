e2d360008ecae57fbed382e42e60cca13e8264e5
Moxirboy <mox777boy@gmail.com>
2023-04-19 10:43:02 +0500

    check

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include<iostream>
+#include<string>
+#include<fstream>
 using namespace std;
+class person{
+private:
+    string ID;
+    string password;
+public:
+person(string id,string password){
+    ID=id;
+    this->password=password;
+    
+}
+};
 int main(){
     cout<<"boshlimizmi???????????";
-
-
-    cout << "qachon ????" << endl;
     system("pause");
     return 0;
-   
 }
