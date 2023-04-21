64d37d2b01e147b28590551fedac873695f1cb66
Moxirboy <mox777boy@gmail.com>
2023-04-21 14:21:34 +0500

    check

--- main.cpp
+++ main.cpp
@@ -2,16 +2,58 @@
 #include<string>
 #include<fstream>
 using namespace std;
-class Person{
-private:
+class person{
+    protected:
     string name;
     string password;
+
+};
+class teacher:person
+{
+    public:
+    void filesaver(string name,string password){
+   ofstream outfile("Teacher", ios::app);
+    if (!outfile.is_open()) {
+        cout << "Failed to open file." << endl;
+    }
+    
+    outfile << name<<endl<<password<<endl;
+    outfile.close();}
+    void filereader(string name,string password){
+        ifstream infile("Teacher",ios::in);
+        string line;
+        bool found=false;
+        bool gound=false;
+        while(getline(infile,line)){
+            if(line.find(name)!=string::npos){
+                found=true;
+                break;
+            }
+        }
+        while(getline(infile,line)){
+            if(line.find(password)!=string::npos){
+                gound=true;
+                break;
+            }
+        }
+        if (found) {
+        cout << "Found the word '" << name << "' in the file." << endl;
+    } else {
+        cout << "Did not find the word '" << name<< "' in the file." << endl;
+    }
+    
+    if (gound) {
+        cout << "Found the word '" << password << "' in the file." << endl;
+    } else {
+        cout << "Did not find the word '" << password<< "' in the file." << endl;
+    }
+    }
+
+};
+
+class student:person
+{
 public:
-Person(){}
-Person(string id,string password){
-    name=id;
-    this->password=password;   
-}
 void filesaver(string name,string password){
    ofstream outfile("person", ios::app);
     if (!outfile.is_open()) {
@@ -52,11 +94,22 @@ void filesaver(string name,string password){
 };
 int main(){
     int a=5;
-    Person person1;
+    student person1;
+    teacher techone;
     string name,password;
+    cout<<"enter name ";
+    cin>>name;
+    cout<<"enter pasword";
+    cin>>password;
+    techone.filesaver(name,password);
     do{
         int l=0;
-        cout<<"Login or Password " <<endl;
+        cout<<"teacher or student "<<endl;
+        cin>>l;
+        switch (l)
+        {
+        case 1:
+               cout<<"Login or sign " <<endl;
         cin>>l;
     switch (l){
         case 1:
@@ -72,10 +125,19 @@ int main(){
     cout<<"Password:";
     cin>>password;
     person1.filereader(name,password);
-        default:
-            break;
-    case 3: exit(0);break;    }
-    } while(a);
+    case 3: exit(0);break;    
+    default:break;}
+    case 2: cout<<"name:";
+    cin>>name;
+    cout<<"Password:";
+    cin>>password;
+    techone.filereader(name,password);
+    
+    
+    default:
+            break;}
+        
+    }while(a);
     system("pause");
     return 0;
 }
