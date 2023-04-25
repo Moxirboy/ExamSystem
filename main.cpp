b3d3acc2aea608e7e6deaa440c4465937f48328e
Moxirboy <mox777boy@gmail.com>
2023-04-25 19:37:00 +0500

    chack

--- main.cpp
+++ main.cpp
@@ -5,142 +5,171 @@
 
 #define Clear system("cls");
 using namespace std;
-class person{
-    protected:
+class essay {
+public: void writer() {
+    ofstream outfile("test.txt", ios::app);
+    if (!outfile.is_open()) {
+        cout << "Failed to open file." << endl;
+    }
+    string c;
+    cout << "name of question" << endl;
+    cin >> c;
+    outfile <<"!!" << c <<"!!" << endl;
+    char ch;
+    cout << "type '$' when it is done" << endl;
+    outfile << "$";
+    do {
+        ch = std::cin.get();
+        outfile.put(ch);
+        
+    } while (ch != '$');
+    outfile.close();
+}
+};
+class person {
+protected:
     string name;
     string password;
 
 };
-class teacher:person
-{
-    public:
-    void filesaver(string name,string password){
-   ofstream outfile("Teacher", ios::app);
-    if (!outfile.is_open()) {
-        cout << "Failed to open file." << endl;
-    }
-    
-    outfile << name<<endl<<password<<endl;
-    outfile.close();}
-    void filereader(string name,string password){
-        ifstream infile("Teacher",ios::in);
-        string line;
-        bool found=false;
-        bool gound=false;
-        while(getline(infile,line)){
-            if(line.find(name)!=string::npos){
-                found=true;
-                break;
-            }
-        }
-        while(getline(infile,line)){
-            if(line.find(password)!=string::npos){
-                gound=true;
-                break;
-            }
-        }
-        if (found) {
-        cout << "Found the word '" << name << "' in the file." << endl;
-    } else {
-        cout << "Did not find the word '" << name<< "' in the file." << endl;
-    }
-    
-    if (gound) {
-        cout << "Found the word '" << password << "' in the file." << endl;
-    } else {
-        cout << "Did not find the word '" << password<< "' in the file." << endl;
-    }
-    }
-
-};
-
-class student:person
+class teacher :person
 {
 public:
-void filesaver(string name,string password){
-   ofstream outfile("person", ios::app);
-    if (!outfile.is_open()) {
-        cout << "Failed to open file." << endl;
+    void filesaver(string name, string password) {
+        ofstream outfile("Teacher", ios::app);
+        if (!outfile.is_open()) {
+            cout << "Failed to open file." << endl;
+        }
+
+        outfile << name << endl << password << endl;
+        outfile.close();
     }
-    
-    outfile << name<<endl<<password<<endl;
-    outfile.close();}
-    void filereader(string name,string password){
-        ifstream infile("person",ios::in);
+    void filereader(string name, string password) {
+        ifstream infile("Teacher", ios::in);
         string line;
-        bool found=false;
-        bool gound=false;
-        while(getline(infile,line)){
-            if(line.find(name)!=string::npos){
-                found=true;
+        bool found = false;
+        bool gound = false;
+        while (getline(infile, line)) {
+            if (line.find(name) != string::npos) {
+                found = true;
                 break;
             }
         }
-        while(getline(infile,line)){
-            if(line.find(password)!=string::npos){
-                gound=true;
+        while (getline(infile, line)) {
+            if (line.find(password) != string::npos) {
+                gound = true;
                 break;
             }
         }
         if (found) {
-        cout << "Found the word '" << name << "' in the file." << endl;
-    } else {
-        cout << "Did not find the word '" << name<< "' in the file." << endl;
+            cout << "Found the word '" << name << "' in the file." << endl;
+        }
+        else {
+            cout << "Did not find the word '" << name << "' in the file." << endl;
+        }
+
+        if (gound) {
+            cout << "Found the word '" << password << "' in the file." << endl;
+        }
+        else {
+            cout << "Did not find the word '" << password << "' in the file." << endl;
+        }
     }
-    
-    if (gound) {
-        cout << "Found the word '" << password << "' in the file." << endl;
-    } else {
-        cout << "Did not find the word '" << password<< "' in the file." << endl;
+
+};
+
+class student :person
+{
+public:
+    void filesaver(string name, string password) {
+        ofstream outfile("person", ios::app);
+        if (!outfile.is_open()) {
+            cout << "Failed to open file." << endl;
+        }
+
+        outfile << name << endl << password << endl;
+        outfile.close();
     }
+    void filereader(string name, string password) {
+        ifstream infile("person", ios::in);
+        string line;
+        bool found = false;
+        bool gound = false;
+        while (getline(infile, line)) {
+            if (line.find(name) != string::npos) {
+                found = true;
+                break;
+            }
+        }
+        while (getline(infile, line)) {
+            if (line.find(password) != string::npos) {
+                gound = true;
+                break;
+            }
+        }
+        if (found) {
+            cout << "Found the word '" << name << "' in the file." << endl;
+        }
+        else {
+            cout << "Did not find the word '" << name << "' in the file." << endl;
+        }
+
+        if (gound) {
+            cout << "Found the word '" << password << "' in the file." << endl;
+        }
+        else {
+            cout << "Did not find the word '" << password << "' in the file." << endl;
+        }
     }
 };
-int main(){
-    int a=5;
-    student person1;
-    teacher techone;
-    string name,password;
-    do{
-        int l;
-        int f;
-        cout<<"1- student or 2-teacher or 0-out "<<endl;
-        cin>>l;
-        Clear
-        switch (l)
-        {
-        case 1:
-        cout<<"1-sign or 2-Login or 0-out " <<endl;
-        cin>>f;
-    switch (f){
-    case 1:
-    cout<<"enter name ";
-    cin>>name;
-    cout<<"enter password ";
-    cin>>password;
-    person1.filesaver(name,password);
-    Clear
-     break;
-    case 2:
-    cout<<"name:";
-    cin>>name;
-    cout<<"Password:";
-    cin>>password;
-    Clear
-    person1.filereader(name,password);
-    break;
-    case 0: exit(0);break;    
-    default:cout<<"invalid";break;} break;
-    case 2: 
-    cout<<"name:";
-    cin>>name;
-    cout<<"Password:";
-    cin>>password;
-    Clear
-    techone.filereader(name,password); break;
-    
-    case 0: exit(0);break;
-    default: cout<<"invalid";break; }}while(a);
-    Clear
+int main() {
+    // int a=5;
+    // student person1;
+    // teacher techone;
+    // string name,password;
+    // do{
+    //     int l;
+    //     int f;
+    //     cout<<"1- student or 2-teacher or 0-out "<<endl;
+    //     cin>>l;
+    //     Clear
+    //     switch (l)
+    //     {
+    //     case 1:
+    //     cout<<"1-sign or 2-Login or 0-out " <<endl;
+    //     cin>>f;
+    // switch (f){
+    // case 1:
+    // cout<<"enter name ";
+    // cin>>name;
+    // cout<<"enter password ";
+    // cin>>password;
+    // person1.filesaver(name,password);
+    // Clear
+    //  break;
+    // case 2:
+    // cout<<"name:";
+    // cin>>name;
+    // cout<<"Password:";
+    // cin>>password;
+    // Clear
+    // person1.filereader(name,password);
+    // break;
+    // case 0: exit(0);break;    
+    // default:cout<<"invalid";break;} break;
+    // case 2: 
+    // cout<<"name:";
+    // cin>>name;
+    // cout<<"Password:";
+    // cin>>password;
+    // Clear
+    // techone.filereader(name,password); break;
+
+    // case 0: exit(0);break;
+    // default: cout<<"invalid";break; }}while(a);
+    // Clear
+    essay s;
+    s.writer();
     system("pause");
     return 0;
 }
