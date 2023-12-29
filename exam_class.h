ecdc3207dae9fb37c07b1dacdadc2b6ad3fc90f5
moxirboy <mox777boy@gmail.com>
2023-12-29 23:34:40 +0500

    deleted

--- exam_class.h
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<string>
-#include<fstream>
-#include<authorization.h>
-#include<essay_class.h>
-#include<obj_class.h>
-using namespace std;
-class exam_system:public essay,public objective,public teacher, public student {
-};
